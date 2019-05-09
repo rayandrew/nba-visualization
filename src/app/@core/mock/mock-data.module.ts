f7d8967786cd2fd58cb38286c29c6433378a5314
Ray Andrew <raydreww@gmail.com>
2019-05-09 15:20:26 +0700

    add logo

--- src/app/@core/mock/mock-data.module.ts
+++ src/app/@core/mock/mock-data.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { SmartTableService } from './smart-table.service';
+// import { SmartTableService } from './smart-table.service';
 import { TeamService } from './team.service';
 import { PlayerService } from './player.service';
 
